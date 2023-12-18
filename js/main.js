0d2a887db6dee14ab58f2d14dd04e2cd8c2f2dc8
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2023-12-18 14:22:21 +0200

    finishin project

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-var productName = document.getElementById("productName")
-var productPrice = document.getElementById("productPrice")
-var productCategory = document.getElementById("productCategory")
-var productSale = document.getElementById("productSale")
-var productDescription = document.getElementById("productDescription")
+var productName = document.getElementById("productName");
+var productPrice = document.getElementById("productPrice");
+var productCategory = document.getElementById("productCategory");
+var productSale = document.getElementById("productSale");
+var productDescription = document.getElementById("productDescription");
+var searchValue = document.getElementById("searchVal");
 var products = [];
-var productUpdate = document.getElementById("update")
-var productEdit = document.getElementById("edit")
+var productUpdate = document.getElementById("update");
+var productEdit = document.getElementById("edit");
 var selectedProductIndex;
 
 if (localStorage.getItem("Data") != null) {
-products = JSON.parse(localStorage.getItem("Data"))
+    products = JSON.parse(localStorage.getItem("Data"));
 
-display()
+    display();
 }
 
 function addProduct() {
@@ -21,64 +22,63 @@ function addProduct() {
         category: productCategory.value,
         sale: productSale.checked,
         description: productDescription.value,
-    }
-    products.push(product) 
-    display()
+    };
+    products.push(product);
+    display();
 
-    localStorage.setItem("Data", JSON.stringify(products))
+    localStorage.setItem("Data", JSON.stringify(products));
 
-    clearForm()
-
-    return product
+    clearForm();
 }
 
 function display() {
-    var result = ``
+    var result = ``;
     for (var i = 0; i < products.length; i++) {
         var productSaleValue;
         if (products[i].sale) {
-            productSaleValue = 'Yes'
+            productSaleValue = "Yes";
         } else {
-            productSaleValue = "No"
+            productSaleValue = "No";
         }
-
-        result += `<tr>
-        <td>`+ (i+1) +`</td>
-        <td>`+ products[i].name +`</td>
-        <td>`+ products[i].price +`</td>
-        <td>`+ products[i].category +`</td>
-        <td>`+ productSaleValue +`</td>
-        <td>`+ products[i].description +`</td>
-        <td> <button onclick="getToUpdateProduct(`+i+`)" class="btn btn-warning"> Update</button> </td>
-        <td> <button onclick="deleteProduct(`+i+`)" class="btn btn-danger"> Delete</button> </td>
-    </tr>`
+        result +=
+            `<tr>
+            <td>` +            (i + 1) +`</td>
+        <td>` +products[i].name +`</td>
+        <td>` +    products[i].price +`</td>
+        <td>` +     products[i].category +`</td>
+        <td>` +productSaleValue +`</td>
+        <td>` +products[i].description +`</td>
+        <td> <button onclick="getToUpdateProduct(` +i +`)" class="btn btn-warning"> Update</button> </td>        <td> <button onclick="deleteProduct(` +i +`)" class="btn btn-danger"> Delete</button> </td>
+    </tr>`;
     }
-    document.getElementById("myData").innerHTML = result
+    document.getElementById("myData").innerHTML = result;
 }
 
 function deleteProduct(i) {
-    products.splice(i,1)
-    display()
-    localStorage.setItem("Data",JSON.stringify(products))
+    products.splice(i, 1);
+    display();
+    localStorage.setItem("Data", JSON.stringify(products));
 }
 
 function clearForm() {
-    productName.value = ""
-    productPrice.value = ""
-    productSale.checked = false
-    productCategory.value = "tv"
-    productDescription.value = ""
+    productName.value = "";
+    productPrice.value = "";
+    productSale.checked = false;
+    productCategory.value = "tv";
+    productDescription.value = "";
 }
 
 function getToUpdateProduct(index) {
-    selectedProductIndex = index
-    productUpdate.classList.remove("d-none")
-    productEdit.classList.add("d-none")
-    productName.value = products[index].name
-    productPrice.value = products[index].price
-    productCategory.value = products[index].category
-    productSale.checked = products[index].sale
-    productDescription.value = products[index].description
+    selectedProductIndex = index;
+
+    productUpdate.classList.remove("d-none");
+    productEdit.classList.add("d-none");
+
+    productName.value = products[index].name;
+    productPrice.value = products[index].price;
+    productCategory.value = products[index].category;
+    productSale.checked = products[index].sale;
+    productDescription.value = products[index].description;
 }
 
 function editProduct() {
@@ -88,12 +88,37 @@ function editProduct() {
         category: productCategory.value,
         sale: productSale.checked,
         description: productDescription.value,
-    }
+    };
     products[selectedProductIndex] = product;
-    console.log(selectedProductIndex,product);
-    localStorage.setItem("Data", JSON.stringify(products))
-    productUpdate.classList.add("d-none")
-    productEdit.classList.remove("d-none")
+    localStorage.setItem("Data", JSON.stringify(products));
+    productUpdate.classList.add("d-none");
+    productEdit.classList.remove("d-none");
     display();
     clearForm();
+}
+
+function search() {
+    var result = ``;
+    var productSaleValue;
+    var searchVal = searchValue.value.toLowerCase();
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].name.toLowerCase().includes(searchVal) || products[i].category.toLowerCase().includes(searchVal)){
+        if (products[i].sale) {
+            productSaleValue = "Yes";
+        } else {
+            productSaleValue = "No";
+        }
+        result +=
+            `<tr>
+            <td>` +            (i + 1) +`</td>
+    <td>` +products[i].name.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
+        <td>` +    products[i].price +`</td>
+        <td>` +products[i].category.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
+        <td>` +productSaleValue +`</td>
+        <td>` +products[i].description +`</td>
+        <td> <button onclick="getToUpdateProduct(` +i +`)" class="btn btn-warning"> Update</button> </td>        <td> <button onclick="deleteProduct(` +i +`)" class="btn btn-danger"> Delete</button> </td>
+    </tr>`;
+        }
+    }
+    document.getElementById("myData").innerHTML = result;
 }
