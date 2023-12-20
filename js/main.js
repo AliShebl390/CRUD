1344e94b0eb35755d98b2c19d2ca0384d9a318cd
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2023-12-20 14:52:17 +0200

    reMake

--- js/main.js
+++ js/main.js
@@ -1,124 +1,118 @@
-var productName = document.getElementById("productName");
-var productPrice = document.getElementById("productPrice");
-var productCategory = document.getElementById("productCategory");
-var productSale = document.getElementById("productSale");
-var productDescription = document.getElementById("productDescription");
-var searchValue = document.getElementById("searchVal");
-var products = [];
-var productUpdate = document.getElementById("update");
-var productEdit = document.getElementById("edit");
-var selectedProductIndex;
+var productName = document.getElementById("productName")
+var productPrice = document.getElementById("productPrice")
+var productCategory = document.getElementById("productCategory")
+var productSale = document.getElementById("productSale")
+var productDescription = document.getElementById("productDescription")
+var pros = []
+var lastData = null
+var searchValue = document.getElementById("searchvalue")
 
-if (localStorage.getItem("Data") != null) {
-    products = JSON.parse(localStorage.getItem("Data"));
-
-    display();
+if (localStorage.getItem("data") != null){
+    pros = JSON.parse(localStorage.getItem("data"))
+    see()
 }
 
-function addProduct() {
-    var product = {
-        name: productName.value,
-        price: Number(productPrice.value),
-        category: productCategory.value,
-        sale: productSale.checked,
-        description: productDescription.value,
-    };
-    products.push(product);
-    display();
-
-    localStorage.setItem("Data", JSON.stringify(products));
-
-    clearForm();
+function addPro() {
+var pro = {
+    name: productName.value,
+    price: Number(productPrice.value),
+    category: productCategory.value,
+    sale: productSale.checked,
+    description: productDescription.value
+}
+    pros.push(pro)
+    localStorage.setItem("data",JSON.stringify(pros))
+    see()
+    clearForm()
 }
 
-function display() {
-    var result = ``;
-    for (var i = 0; i < products.length; i++) {
-        var productSaleValue;
-        if (products[i].sale) {
-            productSaleValue = "Yes";
-        } else {
-            productSaleValue = "No";
-        }
-        result +=
-            `<tr>
-            <td>` +            (i + 1) +`</td>
-        <td>` +products[i].name +`</td>
-        <td>` +    products[i].price +`</td>
-        <td>` +     products[i].category +`</td>
-        <td>` +productSaleValue +`</td>
-        <td>` +products[i].description +`</td>
-        <td> <button onclick="getToUpdateProduct(` +i +`)" class="btn btn-warning"> Update</button> </td>        <td> <button onclick="deleteProduct(` +i +`)" class="btn btn-danger"> Delete</button> </td>
-    </tr>`;
+function see() {
+    var res = ``
+    var saleValue = ''
+    for (let i = 0; i < pros.length; i++) {
+        if (pros[i].sale) {
+            saleValue = "Yes"
+        } else {    saleValue = "No"       }
+        res += `<tr>
+        <td>`+(i+1)+`</td>
+        <td>`+pros[i].name+`</td>
+        <td>`+pros[i].price+`</td>
+        <td>`+pros[i].category+`</td>
+        <td>`+saleValue+`</td>
+        <td>`+pros[i].description+`</td>
+        <td><button onclick="getDataToUpdate(`+i+`)" class="btn btn-outline-warning">Update</button></td>
+        <td><button onclick="del(`+i+`)" class="btn btn-outline-danger">Delete</button></td>
+    </tr>`
     }
-    document.getElementById("myData").innerHTML = result;
+    document.getElementById("data").innerHTML = res
 }
 
-function deleteProduct(i) {
-    products.splice(i, 1);
-    display();
-    localStorage.setItem("Data", JSON.stringify(products));
+function del(i) {
+    pros.splice(i,1)
+    localStorage.setItem("data",JSON.stringify(pros))
+    see()
 }
 
 function clearForm() {
-    productName.value = "";
-    productPrice.value = "";
-    productSale.checked = false;
-    productCategory.value = "tv";
-    productDescription.value = "";
+    document.getElementById("productName").value = ""
+    document.getElementById("productPrice").value = ""
+    document.getElementById("productCategory").value = "tv"
+    document.getElementById("productDescription").value = ""
+    document.getElementById("productSale").checked = false
 }
 
-function getToUpdateProduct(index) {
-    selectedProductIndex = index;
+function getDataToUpdate(i) {
+    lastData = i
 
-    productUpdate.classList.remove("d-none");
-    productEdit.classList.add("d-none");
-
-    productName.value = products[index].name;
-    productPrice.value = products[index].price;
-    productCategory.value = products[index].category;
-    productSale.checked = products[index].sale;
-    productDescription.value = products[index].description;
+    productName.value = pros[i].name 
+    productPrice.value = pros[i].price 
+    productCategory.value = pros[i].category 
+    productSale.checked = pros[i].sale 
+    productDescription.value = pros[i].description 
+    document.getElementById("edit").classList.remove("d-none")
+    document.getElementById("add").classList.add("d-none")
 }
 
-function editProduct() {
-    var product = {
+function editPro() {
+    var pro = {
         name: productName.value,
         price: Number(productPrice.value),
         category: productCategory.value,
         sale: productSale.checked,
-        description: productDescription.value,
-    };
-    products[selectedProductIndex] = product;
-    localStorage.setItem("Data", JSON.stringify(products));
-    productUpdate.classList.add("d-none");
-    productEdit.classList.remove("d-none");
-    display();
-    clearForm();
-}
-
-function search() {
-    var result = ``;
-    var productSaleValue;
-    var searchVal = searchValue.value.toLowerCase();
-    for (var i = 0; i < products.length; i++) {
-        if (products[i].name.toLowerCase().includes(searchVal) || products[i].category.toLowerCase().includes(searchVal)){
-        if (products[i].sale) {
-            productSaleValue = "Yes";
-        } else {
-            productSaleValue = "No";
-        }
-        result +=
-            `<tr>
-            <td>` +            (i + 1) +`</td>
-    <td>` +products[i].name.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
-        <td>` +    products[i].price +`</td>
-        <td>` +products[i].category.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
-        <td>` +productSaleValue +`</td>
-        <td>` +products[i].description +`</td>
-        <td> <button onclick="getToUpdateProduct(` +i +`)" class="btn btn-warning"> Update</button> </td>        <td> <button onclick="deleteProduct(` +i +`)" class="btn btn-danger"> Delete</button> </td>
-    </tr>`;
-        }
+        description: productDescription.value
     }
-    document.getElementById("myData").innerHTML = result;
-}
+        pros[lastData] = pro
+        localStorage.setItem("data",JSON.stringify(pros))
+        document.getElementById("add").classList.remove("d-none")
+        document.getElementById("edit").classList.add("d-none")
+        see()
+        clearForm()
+    }
+    function search() {
+        var res = ``
+        var searchVal = searchValue.value.toLowerCase()
+        var saleValue = ""
+        for (let i = 0; i < pros.length; i++) {
+            if (pros[i].sale) {
+                saleValue = "Yes"
+            } else {    saleValue = "No"}
+            if (pros[i].name.toLowerCase().includes(searchVal) || pros[i].category.toLowerCase().includes(searchVal))
+            res += `<tr>
+            <td>`+(i+1)+`</td>
+            <td>`+pros[i].name.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
+            <td>`+pros[i].price+`</td>
+            <td>`+pros[i].category.toLowerCase().replace(searchVal,"<span class='bg-info'>"+searchVal+"</span>") +`</td>
+            <td>`+saleValue+`</td>
+            <td>`+pros[i].description+`</td>
+            <td><button onclick="getDataToUpdate(`+i+`)" class="btn btn-outline-warning">Update</button></td>
+            <td><button onclick="del(`+i+`)" class="btn btn-outline-danger">Delete</button></td>
+        </tr>`
+        }
+        document.getElementById("data").innerHTML = res
+    }
+
+    function removeAll() {
+        pros = []
+        localStorage.setItem("data",JSON.stringify(pros))
+        see()
+    }
